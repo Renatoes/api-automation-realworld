c91202b73d3748bb0d63d9590506ac10d39573aa
Renatoes <renciks59@gmail.com>
2020-08-03 15:28:05 +0300

    DID all tests cases

--- src/test/java/com/testing/serenitySteps/RealworldSteps.java
+++ src/test/java/com/testing/serenitySteps/RealworldSteps.java
@@ -15,9 +15,10 @@ import static net.serenitybdd.core.Serenity.setSessionVariable;
 
 public class RealworldSteps extends BaseSteps {
   private final static String _API_USERS_ = "/api/users/";
+  private final static String _API_USER_ = "/api/user/";
   private final static String _API_ARTICLES_ = "/api/articles/";
-  // Find endpoint for API login
-  private final static String _API_USERS_LOGIN_ = null;
+  //Find endpoint for API login
+  private final static String _API_USERS_LOGIN_ = "/api/users/login/";
 
   @Steps
   RealworldSteps realworldSteps;
@@ -59,6 +60,38 @@ public class RealworldSteps extends BaseSteps {
     }
   }
 
+  @Step
+  public static void updateUserProfile(DataTable dataTable) throws IOException {
+    sendRequestWithBodyJson(PUT,_API_USER_,
+            createBody(dataTable));
+  }
+
+  @Step
+  public static void createPost(DataTable dataTable) throws IOException {
+    sendRequestWithBodyJson(POST,_API_ARTICLES_,
+            createBody(dataTable));
+  }
+  @Step
+  public static void deletePost() {
+    sendRequest(DELETE ,_API_ARTICLES_ + sessionVariableCalled("slug"));
+  }
+
+  @Step
+  public static void modifyPost(DataTable dataTable) throws IOException {
+    sendRequestWithBodyJson(PUT,_API_ARTICLES_+ sessionVariableCalled("slug"),
+            createBody(dataTable));
+  }
+  @Step
+  public static void addingComment(DataTable dataTable) throws IOException {
+    sendRequestWithBodyJson(POST,_API_ARTICLES_+ sessionVariableCalled("slug") + "/comments/",
+            createBody(dataTable));
+  }
+
+  @Step
+  public static void deleteComment() {
+    // /api/articles/hiy2h1y3uyh2iuh31a/comments/3
+    sendRequest(DELETE,_API_ARTICLES_ + sessionVariableCalled("slug") + "/comments/" + sessionVariableCalled("comment_id"));
+  }
 
   // Private
 
